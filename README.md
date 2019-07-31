e27521c55b7994a148d5225b77655d62102a04f5
joseigor <joseigorcfm@gmail.com>
2019-07-31 19:31:48 -0300

    webpack configuration of the project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# gaugeiot-frontend
-Repository for the web application of Gauge Iot.
+![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)
+
+# personal_website
+This is my personal website project.
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Button from "./Button/Button";
+
+import "../../assets/sass/main.scss";
+
+const App = () => {
+  return (
+    <>
+      <div>test</div>
+    </>
+  );
+};
+
+export default App;
